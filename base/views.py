ec362172053f711af66b140be09f1302b9db3dbb
Anand Kumar Mishra <ctfu.anand@outlook.com>
2023-11-16 21:02:15 +0530

    init

---
+++ base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design With Me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
+
+
+def home(request):
+    return render(request, 'home.html', {'rooms': rooms})
+
+
+def room(request):
+    return render(request, 'room.html', {'rooms': rooms})
