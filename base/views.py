b638c5ae52d1e71ee63c9f6b73c49f5cf2f598a1
Anand Kumar Mishra <ctfu.anand@outlook.com>
2023-11-17 20:43:45 +0530

    messages CRUD implemented

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import RoomForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -85,7 +85,17 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
-    context = {'room': room, 'room_messages': room_messages}
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -133,3 +143,16 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj': room})
+
+
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room', pk=message.room.id)
+
+    return render(request, 'base/delete.html', {'obj': message})
