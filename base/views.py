f8211d698b1d4a041a9c4030d42f771bc5e525b4
Anand Kumar Mishra <ctfu.anand@outlook.com>
2023-11-18 10:30:42 +0530

    activity feed done

--- base/views.py
+++ base/views.py
@@ -78,7 +78,8 @@ def home(request):
     )
     room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'q': q}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'q': q, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
